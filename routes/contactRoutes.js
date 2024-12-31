cb1f7196e4fd015db1fc6354a39a1813f98016d5
bguptaping <bgupta.ping@gmail.com>
2024-12-31 11:10:32 +0530

    intriduce error handling

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,30 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-    res.status(200).json({
-        message: "get cont"
-    })
-})
-router.route('/:id').get((req, res) => {
-    res.status(200).json({
-        message: `get  ${req.params.id}`
-    })
-})
-router.route('/').post((req, res) => {
-    res.status(200).json({
-        message: "create"
-    })
-})
-router.route('/:id').put((req, res) => {
-    res.status(200).json({
-        message: `update ${req.params.id}`
-    })
-})
-router.route('/:id').delete((req, res) => {
-    res.status(200).json({
-        message: `remove  ${req.params.id}`
-    })
-})
+const { getContacts, getContact, createContact, updateContact, deleteContact } = require('../controllers/contactController')
+
+router.route('/').get(getContacts).post(createContact);
+router.route('/:id').get(getContact).delete(deleteContact).put(updateContact)
 
 module.exports = router;
